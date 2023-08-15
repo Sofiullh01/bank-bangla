5cb669da90c84f97901309efeb1c5b1c82e8f987
Sofiullah01 <ssisofiulislam@gmail.com>
2023-08-15 13:36:53 +0600

    complet the bank site

---
+++ js/depo.js
@@ -0,0 +1,49 @@
+document.getElementById('dpoBtn').addEventListener('click', function(){
+    
+    const depoInput = document.getElementById('depoInput');
+    const newDepositAmontString = depoInput.value ;
+    const newDepositAmont = parseFloat(newDepositAmontString);
+    
+    
+    const displayDepoSum = document.getElementById('displayDepoSum');
+    const displayDepoSumAmontString = displayDepoSum.innerText;
+    const displayDepoSumAmont = parseFloat(displayDepoSumAmontString)
+
+    const currentDepositSum = newDepositAmont + displayDepoSumAmont;
+
+    displayDepoSum.innerText = currentDepositSum;
+    depoInput.value = '';
+
+    const displayTotaSum = document.getElementById('displayTotaSum');
+    const displayTotaSumString = displayTotaSum.innerText;
+    const convertDisplayTotaSumString = parseFloat(displayTotaSumString)
+
+    const blanceTotal = convertDisplayTotaSumString +  newDepositAmont;
+    
+    displayTotaSum.innerText = blanceTotal;
+})
+
+// widthro----------------------------------------
+document.getElementById('widtBtn').addEventListener('click', function(){
+    
+    const withdrawInput = document.getElementById('WithdrawInput');
+    const newWithdrawInputString = withdrawInput.value ;
+    const newWithdrawAmont = parseFloat(newWithdrawInputString);
+
+    const displayWirSum = document.getElementById('displayWirSum');
+    const displayWirSumString = displayWirSum.innerText;
+    const displayWirSumAmont = parseFloat(displayWirSumString)
+
+    const currentWithdrSum = newWithdrawAmont + displayWirSumAmont;
+
+    displayWirSum.innerText = currentWithdrSum ;
+    withdrawInput.value = '';
+
+    const displayTotaSum = document.getElementById('displayTotaSum');
+    const displayTotaSumString = displayTotaSum.innerText;
+    const convertDisplayTotaSumString = parseFloat(displayTotaSumString)
+
+    const blanceTotal = convertDisplayTotaSumString - newWithdrawAmont;
+    
+    displayTotaSum.innerText = blanceTotal;
+})
